9f97f60f00670c245b35fdf13b5292035845202c
pikl <pikl@oksystem.cz>
2017-05-10 08:29:25 +0200

    0.0.1

---
+++ DI/ConfigParamsExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Pikl\ConfigParams\DI;
+
+use Nette\DI\Compiler;
+use Nette\DI\CompilerExtension;
+
+final class ConfigParamsExtension extends CompilerExtension {
+
+    public function loadConfiguration() {
+        Compiler::loadDefinitions(
+            $this->getContainerBuilder(),
+            $this->loadFromFile(__DIR__.'/../config/services.neon')
+        );
+    }
+
+}
---
+++ src/ConfigParams.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Pikl\ConfigParams;
+
+final class ConfigParams {
+
+	/** @var \Nette\DI\Container */
+	private $context;
+
+	public function __construct(\Nette\DI\Container $context = NULL) {
+		$this->context = $context;
+	}
+
+	public function getDebugMode() {
+		return $this->context->parameters['debugMode'];
+	}
+
+	public function getProductionMode() {
+		return $this->context->parameters['productionMode'];
+	}
+
+	public function getConsoleMode() {
+		return $this->context->parameters['consoleMode'];
+	}
+
+}
